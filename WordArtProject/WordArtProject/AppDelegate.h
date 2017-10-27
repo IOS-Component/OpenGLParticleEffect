439efff202c7655987b8cdb7155de32dfa34a98b
张雅彬 981213 <richsjeson@gmail.com>
2017-10-27 11:48:05 +0800

    OpenGL艺术字效果
    在绘制艺术字的过程中，显示粒子和阴影效果，并自动截取图片生成Gif

---
+++ WordArtProject/WordArtProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WordArtProject
+//
+//  Created by richsjeson on 2017/10/27.
+//  Copyright © 2017年 richsjeson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WordArtProject/WordArtProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WordArtProject
+//
+//  Created by richsjeson on 2017/10/27.
+//  Copyright © 2017年 richsjeson. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
